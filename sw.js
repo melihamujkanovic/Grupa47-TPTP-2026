291f73cc6ce26fb4fc7b95ec73befbc9b007c9f6
melihamujkanovic <mely.mosnik2@gmail.com>
2026-05-15 19:04:32 +0200

    Ažurirana Service Worker registracija i cache logika, poboljšano rukovanje error-ima

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-const cacheName = 'sola-v1';
+const cacheName = 'sola-v2';
 const staticAssets = [
   './',
   './index.html',
   './sadrzaj.html',
+  './kontakt.html',
   './css/tptpstil.css',
   './js/tptpskripte.js',
   './images/haljina.jpg',
@@ -17,13 +18,43 @@ const staticAssets = [
 
 // Instalacija i keširanje resursa
 self.addEventListener('install', async e => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
+  try {
+    const cache = await caches.open(cacheName);
+    // Cachiramo resurse individualno da izbegnemo grešku ako jedan ne postoji
+    for (const asset of staticAssets) {
+      try {
+        await cache.add(asset);
+      } catch (err) {
+        console.warn('Nije moguće cachirati resurc:', asset, err);
+      }
+    }
+    self.skipWaiting(); // Aktiviramo SW odmah
+  } catch (err) {
+    console.error('Greška pri kešovanju resursa:', err);
+  }
 });
 
 // Slušanje zahtjeva i serviranje iz keša ako nema mreže
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+      .catch(err => {
+        console.warn('Fetch failed for:', e.request.url, err);
+        return new Response('Nema dostupnog resursa', { status: 503 });
+      })
   );
+});
+
+// Aktivacija i brisanje starih cache verzija
+self.addEventListener('activate', async e => {
+  const cacheNames = await caches.keys();
+  await Promise.all(
+    cacheNames.map(name => {
+      if (name !== cacheName) {
+        return caches.delete(name);
+      }
+    })
+  );
+  self.clients.claim();
 });
